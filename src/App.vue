7bef6ed295a939222566d9a80d69e40728193a7a
ziaho <rlawlgh1100@naver.com>
2024-06-12 23:14:45 +0900

    feature / Vue 3강 : v-for 반복문 사용하기

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <template>
+
+  <div class="menu">
+    <a v-for="(menu, index) in menus" :key="index">{{ menu }}</a>
+  </div>
+
   <img alt="Vue logo" src="./assets/logo.png">
   <div>
-    <h4 :style="style">XX 원룸</h4>
+    <h4 :style="blueFont">{{ products[0] }}</h4>
     <p>{{ price1 }} 만원</p>
   </div>
   <div>
-    <h4>XX 원룸</h4>
+    <h4>{{ products[1] }}</h4>
     <p>{{ price2 }} 만원</p>
+  </div>
+  <div>
+    <h4>{{ products[2] }}</h4>
+    <p>70만원</p>
   </div>
   
 </template>
@@ -18,9 +27,11 @@ export default {
   name: 'App',
   data() {
     return {
-      price1 : 30,
-      price2 : 50,
-      style : 'color : blue'
+      price1: 30,
+      price2: 50,
+      blueFont: 'color : blue',
+      products: ['역삼동원룸', '천호동원롬', '마포구원룸'],
+      menus: ['Home', 'Shop', 'About'],
     }
   },
   components: {
@@ -36,5 +47,16 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a {
+  color: white;
+  padding: 10px;
 }
 </style>
