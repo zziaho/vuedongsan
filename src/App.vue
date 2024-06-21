afd71bc4e7d233540ee4c1141dced6dbc926ff4e
ziaho <rlawlgh1100@naver.com>
2024-06-21 23:45:03 +0900

    Vue 5강 : 모달창 만들기

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <template>
 
+  <div class="black-bg" v-if="isModalOpen">
+    <div class="white-bg">
+      <h4>상세페이지</h4>
+      <p>상세페이지 내용</p>
+    </div>
+  </div>
+
   <div class="menu">
     <a v-for="(menu, index) in menus" :key="index">{{ menu }}</a>
   </div>
 
-  <img alt="Vue logo" src="./assets/logo.png">
 
   <div v-for="(product, index) in products" :key="index">
-    <h4>{{ product.name }}</h4>
+    <img :src="(require(`./assets/room${index}.jpg`))" class="room-img">
+    <h4 @click="modalOpen">{{ product.name }}</h4>
     <p>{{ product.price }}</p>
-    <button @:click="increseReportCnt(index)">허위매물신고</button> <span>신고수 : {{ product.reportCnt }}</span>
+    <button @:click="increseReportCnt(index)">허위매물신고</button>
+    <span>신고수 : {{ product.reportCnt }}</span>
   </div>
   
 </template>
@@ -26,26 +34,35 @@ export default {
           name: '역삼동원룸',
           price: '30만원',
           reportCnt: 0,
+          imgUrl: './assets/room',
+          ext: '.jpg',
         },
         {
           name: '천호동원룸',
           price: '50만원',
           reportCnt: 0,
+          imgUrl: './assets/room',
+          ext: '.jpg',
         },
         {
           name: '마포구원룸',
           price: '70만원',
           reportCnt: 0,
+          imgUrl: './assets/room',
+          ext: '.jpg',
         },
       ],
       menus: ['Home', 'Shop', 'About'],
-      reportCnt: 0,
+      isModalOpen: false,
     }
   },
   methods: {
     increseReportCnt(index) {
       this.products[index].reportCnt += 1;
     },
+    modalOpen() {
+      this.isModalOpen = true;
+    }
   },
   components: {
   }
@@ -68,8 +85,34 @@ export default {
   border-radius: 5px;
 }
 
+.room-img {
+  width: 60%;
+  margin-top: 40px;
+}
+
 .menu a {
   color: white;
   padding: 10px;
+}
+
+body {
+  margin : 0
+}
+
+div {
+  box-sizing: border-box;
+}
+
+.black-bg {
+  width: 100%; height: 100%;
+  background: rgba(0,0,0,0.5);
+  position: fixed; padding: 20px;
+}
+
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
 }
 </style>
